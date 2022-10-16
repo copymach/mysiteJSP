f64ea8d20f977f617a56931b7c320ddede8056e2
copymach <copymach@naver.com>
2022-10-16 16:57:12 +0900

    mySite for Jsp QueryDSL.

---
+++ src/main/java/com/example/mysitejsp/MysiteJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysitejsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysiteJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysiteJspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysitejsp/MysiteJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysitejsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysiteJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
